69fda6d41d5147214b7eb85a54da18b042ff46e3
Mohamed Moulay <moulaymohamed856@gmail.com>
2024-11-15 01:07:35 +0100

    add prod links

--- backend/main.py
+++ backend/main.py
@@ -11,8 +11,8 @@ with open(model_path, 'rb') as f:
 origins = [
     "http://localhost:5173",
     "localhost:5173",
-    "https://credit-risk-ensa.netlify.app/",
-    "credit-risk-ensa.netlify.app/"
+    "https://credit-risk-ensa.netlify.app",
+    "credit-risk-ensa.netlify.app"
 ]
 
 
